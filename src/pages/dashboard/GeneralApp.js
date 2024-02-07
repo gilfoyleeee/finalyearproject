420786bc38ebef4439ca3fbaca3564d14d2d85b8
gilfoyleeee <kushalthapa023@gmail.com>
2024-02-07 18:21:30 +0545

    gfgfg

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -9,15 +9,15 @@ import { useSelector } from "react-redux";
 
 const GeneralApp = () => {
 const theme = useTheme();
-const {sideBar} = useSelector((store) => store.app);
-console.log(sideBar);
+const {sidebar} = useSelector((store) => store.app);
+
   return (
     <Stack direction={"row"} sx={{ width: "100%" }}>
       <Chats />
-      <Box sx={{ height: "100%", width: sideBar.open ? "calc(100vw - 740px)": "calc(100vw - 420px)", backgroundColor: theme.palette.mode === "light" ? "#F0F4FA" : theme.palette.background.paper  }}>
+      <Box sx={{ height: "100%", width: sidebar.open ? "calc(100vw - 740px)": "calc(100vw - 420px)", backgroundColor: theme.palette.mode === "light" ? "#F0F4FA" : theme.palette.background.paper  }}>
 <Conversation />
       </Box>
-      {sideBar.open && <Contact />}
+      {sidebar.open && <Contact />}
     </Stack>
   );
 };
